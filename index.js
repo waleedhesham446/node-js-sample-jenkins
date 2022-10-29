7e13791be5c1ce3d8910b63d5e67e3ae4975652d
0xWaleedH <72695729+waleedhesham446@users.noreply.github.com>
2022-10-30 00:01:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<h1>Hello Testing DevOps Node! JS Project Hello How are you ? welcome to devOps Project</h1>')
+  response.send('<h1>Hello Testing DevOps Node! JS Project Hello How are you?</h1>')
 }) 
 
 app.listen(app.get('port'), function() {
